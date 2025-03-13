8ae77f7b35198aadd1b900e5ec01dd88a810f3cd
Alex Newcomer <alexmnewcomer@gmail.com>
2025-03-12 21:12:48 -0400

    created a index.ts file in the models folder

---
+++ src/models/index.ts
@@ -0,0 +1,7 @@
+// Import all models here
+import User from './User';
+import Thought from './Thought';
+// Import other models if you have them, like Application or Reaction
+
+// Export them for easy access from other files
+export { User, Thought };
